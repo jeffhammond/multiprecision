2c435a8d913e91affeaed376941bddb670048738
Jeff Hammond <jeff.science@gmail.com>
2014-03-24 11:58:25 -0500

    add usage of pi

--- boost/basic.cc
+++ boost/basic.cc
@@ -15,18 +15,22 @@ typedef boost::multiprecision::number<cpp_dec_float<64> > mp_type;
 
 int main(void)
 {
-    mp_type a,b,c;
-
-    a = 5;
-    b = 3.14;
-    c = 1./7.;
-
     /* prints the numerical precision i.e. 64 */
     std::cout << std::numeric_limits<mp_type>::digits10 << std::endl;
+
+    mp_type a = 2;
+    mp_type b = boost::math::constants::pi<mp_type, boost::math::policies::policy<boost::math::policies::digits2<64> > >();
+    mp_type c = pow(b,a);
+    mp_type d = exp(a);
+    mp_type e = 1/b;
+    mp_type f = 1./b;
 
     std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << a << std::endl;
     std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << b << std::endl;
     std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << c << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << d << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << e << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << f << std::endl;
 
     return 0;
 }
