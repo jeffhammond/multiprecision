3b6b92cb38deb8f2c5737331ae495f1f75a45fc1
Jeff Hammond <jeff.science@gmail.com>
2015-02-28 21:17:34 -0800

    modern Fortran style in dot

---
+++ blas-like/dot.f90
@@ -0,0 +1,56 @@
+subroutine mydot32(n,x,jx,y,jy,z)
+    use ISO_FORTRAN_ENV
+    implicit none
+    integer, intent(in) :: n,jx,jy
+    integer :: i
+    real(kind=REAL32), intent(in) :: x(n),y(n)
+    real(kind=REAL32), intent(out) :: z
+    real(kind=REAL32) :: r
+    if ((jx.ne.1).or.(jy.ne.1)) then
+        print*,'mysdot32 supports stride 1 only'
+        call abort
+    endif
+    do i=1,n
+        r = r + x(i) * y(i)
+    enddo
+    z = r
+    return
+end subroutine mydot32
+
+subroutine mydot64(n,x,jx,y,jy,z)
+    use ISO_FORTRAN_ENV
+    implicit none
+    integer, intent(in) :: n,jx,jy
+    integer :: i
+    real(kind=REAL64), intent(in) :: x(n),y(n)
+    real(kind=REAL64), intent(out) :: z
+    real(kind=REAL64) :: r
+    if ((jx.ne.1).or.(jy.ne.1)) then
+        print*,'mysdot64 supports stride 1 only'
+        call abort
+    endif
+    do i=1,n
+        r = r + x(i) * y(i)
+    enddo
+    z = r
+    return
+end subroutine mydot64
+
+subroutine mydot128(n,x,jx,y,jy,z)
+    use ISO_FORTRAN_ENV
+    implicit none
+    integer, intent(in) :: n,jx,jy
+    integer :: i
+    real(kind=REAL128), intent(in) :: x(n),y(n)
+    real(kind=REAL128), intent(out) :: z
+    real(kind=REAL128) :: r
+    if ((jx.ne.1).or.(jy.ne.1)) then
+        print*,'mysdot128 supports stride 1 only'
+        call abort
+    endif
+    do i=1,n
+        r = r + x(i) * y(i)
+    enddo
+    z = r
+    return
+end subroutine mydot128
