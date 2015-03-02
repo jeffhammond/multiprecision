a62c939dcafd2189fc31288a2f0d661f1361d4ac
Jeff Hammond <jeff.science@gmail.com>
2015-03-01 22:00:28 -0800

    support GCC toolchain

--- blas-like/quad.h
+++ blas-like/quad.h
@@ -1,10 +1,12 @@
 #ifndef QUAD_H_DEFINED
 #define QUAD_H_DEFINED
 
-#if defined(__INTEL_COMPILER) && 0
-/* This is no longer supported in version 15 it seems. */
-typedef _Quad quad;
+#if defined(__INTEL_COMPILER)
+/* This is no longer supported in version 15 it seems.
+typedef _Quad quad; */
+typedef __float128 quad;
 #elif defined(__GNUC__)
+#include <quadmath.h>
 typedef __float128 quad;
 #else
 #error No 128-bit float type available!
