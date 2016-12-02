a9f0f175a8793f30d37c2e1f14265bf6e5779b58
Jeff Hammond <jeff.science@gmail.com>
2016-12-02 11:08:49 +0800

    use template to test all variants at once

--- boost/basic.cc
+++ boost/basic.cc
@@ -26,33 +26,37 @@ using boost::multiprecision::cpp_bin_float_double;
 using boost::multiprecision::cpp_bin_float_double_extended;
 using boost::multiprecision::cpp_bin_float_quad;
 
-//typedef boost::multiprecision::number<cpp_dec_float<64> > mp_type;
 
-// This is busted: https://svn.boost.org/trac/boost/ticket/11764
-//typedef boost::multiprecision::cpp_bin_float_single mp_type;
+template <typename T>
+void foo(void)
+{
+    std::cout << "========================" << std::endl;
 
-//typedef boost::multiprecision::cpp_bin_float_double mp_type;
-//typedef boost::multiprecision::cpp_bin_float_double_extended mp_type;
-//typedef boost::multiprecision::cpp_bin_float_quad mp_type;
-typedef cpp_bin_float_double_double mp_type;
+    /* prints the numerical precision i.e. 64 */
+    std::cout << std::numeric_limits<T>::digits10 << std::endl;
+
+    T a = 2;
+    //T b = boost::math::constants::pi<T, boost::math::policies::policy<boost::math::policies::digits2<64> > >();
+    T b = boost::math::constants::pi<T>();
+    T c = exp(a);
+    T d = pow(c,c);
+    T e = 1./c;
+
+    std::cout << std::setprecision(std::numeric_limits<T>::max_digits10) << a << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<T>::max_digits10) << b << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<T>::max_digits10) << c << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<T>::max_digits10) << d << std::endl;
+    std::cout << std::setprecision(std::numeric_limits<T>::max_digits10) << e << std::endl;
+}
 
 int main(void)
 {
-    /* prints the numerical precision i.e. 64 */
-    std::cout << std::numeric_limits<mp_type>::digits10 << std::endl;
-
-    mp_type a = 2;
-    //mp_type b = boost::math::constants::pi<mp_type, boost::math::policies::policy<boost::math::policies::digits2<64> > >();
-    mp_type b = boost::math::constants::pi<mp_type>();
-    mp_type c = exp(a);
-    mp_type d = pow(c,c);
-    mp_type e = 1./c;
-
-    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << a << std::endl;
-    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << b << std::endl;
-    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << c << std::endl;
-    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << d << std::endl;
-    std::cout << std::setprecision(std::numeric_limits<mp_type>::max_digits10) << e << std::endl;
+    //foo<boost::multiprecision::cpp_bin_float_single >(); // see bug.cc
+    foo<boost::multiprecision::cpp_bin_float_double >();
+    foo<boost::multiprecision::cpp_bin_float_double_extended >();
+    foo<cpp_bin_float_double_double >();
+    foo<boost::multiprecision::cpp_bin_float_quad >();
+    foo<boost::multiprecision::number<cpp_dec_float<64> > >();
 
     return 0;
 }
